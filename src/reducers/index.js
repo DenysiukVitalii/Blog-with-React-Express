85597e2e5be7bcf7aa5ff36d6c8c159bc6ff95ca
vitalii_denysiuk <dendoc97@gmail.com>
2017-02-05 23:07:42 +0200

    reducers for news API

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import visibilityFilter from './visibilityFilter';
+import selectedSource from './selectedSource';
+import articles from './articles';
 
 const blogApp = combineReducers({
-	visibilityFilter
+	visibilityFilter,
+	selectedSource,
+	articles
 })
 
 export default blogApp;
