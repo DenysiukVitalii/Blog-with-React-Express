4d53326e70620a13bfbc481fd2f64c67d7a14dd4
vitalii_denysiuk <dendoc97@gmail.com>
2017-02-14 23:06:15 +0200

    change PageNewsContainer

--- src/containers/PageNewsContainer.js
+++ src/containers/PageNewsContainer.js
@@ -3,14 +3,28 @@ import PageNews from '../components/PageNews';
 
 class PageNewsContainer extends Component {
 
+  getPostById = (id) => {
+		var myHeaders = new Headers({
+		    "Content-Type": "json/plain",
+		    "X-Custom-Header": "ProcessThisImmediately",
+		});
+
+
+		return fetch(`http://localhost:9000/posts/${id}`,{ method: 'GET',
+               mode: 'cors',
+               cache: 'default' })
+      	.then(response => response.json())
+	};
+
+
     render() {
         return (
         	<div>
-    			 <PageNews postId={this.props.params.postId}/>
+    			 <PageNews postId={this.props.params.postId} getPostById={this.getPostById}/>
   			  </div>
         );
     }
 }
 
 
-export default PageNewsContainer; 
+export default PageNewsContainer;
