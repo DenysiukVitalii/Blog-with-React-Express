372f5d66105b87e1c983ae423c611d0842188c84
vitalii_denysiuk <dendoc97@gmail.com>
2017-02-16 22:29:59 +0200

    change PageNews, add styles

--- src/components/PageNews.js
+++ src/components/PageNews.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import './grid.css';
 import './post.css';
+import './PageNews.css';
 
 class PageNews extends Component {
   constructor(props) {
@@ -15,32 +16,22 @@ class PageNews extends Component {
 	}
     render() {
         return (
-          <div className="gitem-lg-2 post">
-           <div className="post">
-
-              <div className="grid">
-                <div className="gitem-lg-12">
-                  <img src={this.state.image} className="imgPost" alt=""/>
-                </div>
-              </div>
-
-              <div className="grid post-info">
-                <div className="gitem-lg-6">
-                  <span className="review">{this.state.views}</span>
-                </div>
-                <div className="gitem-lg-6 text-right">
-                  <time className="date" dateTime={ this.state.date }>{ this.state.date }</time>
-                </div>
-              </div>
-
-              <div className="grid">
-                <div className="gitem-lg-12">
-                  <a href="" className="post-text">{this.state.header}</a>
-                </div>
-              </div>
-
-           </div>
-        </div>
+          <div className="PageNews">
+		      	<div>
+					<p className="title">{ this.state.title }</p>
+					<div>
+						<div className="bg-wrap"></div>
+						<img src={ this.state.image } alt="news"></img>
+						<p className="stats">
+							<span className="views">
+								<span className="n-views">{ this.state.views }</span>
+							</span>
+							<time dateTime={ this.state.date }>{ this.state.date }</time>
+						</p>
+					</div>
+		      		<p className="news-desc" dangerouslySetInnerHTML={{__html: this.state.describe}}></p>
+		      	</div>
+	      	</div>
          );
      }
 }
